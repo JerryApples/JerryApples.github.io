3bc185ffa0cf449f096f0421923b95a5d7db95cd
JerryApples <99360025+JerryApples@users.noreply.github.com>
2022-10-05 07:39:33 +0300

    Update xryst-2-2-main-js.js

--- xryst-2-2-main-js.js
+++ xryst-2-2-main-js.js
@@ -1,6 +1,10 @@
 const menuButton = document.querySelector('.popup-menu-button');
 const popUpMenu = document.querySelector('.popup-menu');
 const popUpMenuOptions = document.getElementsByClassName('popup-a');
+const seemoreButton = document.querySelector('.seemore');
+const galleryLayout = document.querySelector('.gallery-layout');
+const buttonText = document.querySelector('.button-text');
+const buttonArrow = document.querySelector('.arrow');
 
 function openPopupMenu() {
     document.querySelector('#nav-icon4').classList.toggle('open');
@@ -11,14 +15,23 @@ function openPopupMenu() {
         popUpMenu.classList.add('open-popup');
         querySelector(body).style.overflow = 'hidden';
     }
-}
+};
 
 menuButton.addEventListener('click', openPopupMenu);
 
-function hidePopUpMenu() {
-    popUpMenu.classList.remove('open-popup');
-}
-
 for (i=0; i < popUpMenuOptions.length; i++) {
     popUpMenuOptions[i].addEventListener('click', openPopupMenu);
+};
+
+function openGallery() {
+    if (galleryLayout.classList.contains('show-gallery')) {
+        galleryLayout.classList.remove('show-gallery');
+        buttonText.textContent = 'See More';
+    }
+    else {
+        galleryLayout.classList.add('show-gallery');
+        buttonText.textContent = 'See Less';
+    }
 }
+
+seemoreButton.addEventListener('click', openGallery);
